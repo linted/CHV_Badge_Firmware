70b01c3b5e40bf390ac1a1ef99c2850d422c89dc
linted <linted@users.noreply.github.com>
2022-10-27 15:59:51 -0400

    fixed build script to complain if missing cmake or gcc

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+MISSING_DEPS=0
+if [ ! $(which cmake) ]; then
+  echo "Missing cmake!"
+  MISSING_DEPS=1
+fi
+if [ ! $(which arm-none-eabi-gcc) ]; then
+  echo "missing arm gcc!"
+  MISSING_DEPS=1
+fi
+
+if [ $MISSING_DEPS ]; then
+  exit 1
+fi
+
 pushd $(dirname -- "${BASH_SOURCE[0]}")
 
   mkdir -p build
