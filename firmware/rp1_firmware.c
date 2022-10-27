2ed6b7d948cb19fe745c719f858cc759f4849d4d
linted <linted@users.noreply.github.com>
2022-10-27 16:19:28 -0400

    updated rp1 firmware to send a message every second on the canbus

--- firmware/rp1_firmware.c
+++ firmware/rp1_firmware.c
@@ -1,5 +1,61 @@
+#include <pico/stdlib.h>
+#include <can2040.h>
 
+#define CAN_RX 4; //TODO: change to correct pins
+#define CAN_TX 5; //TODO: change to correct pins
+
+static struct can2040 cbus;
+
+static void
+can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
+{
+    // Add message processing code here...
+}
+
+static void
+PIOx_IRQHandler(void)
+{
+    can2040_pio_irq_handler(&cbus);
+}
+
+void
+canbus_setup(void)
+{
+    uint32_t pio_num = 0;
+    uint32_t sys_clock = 125000000
+    uint32_t bitrate = 500000;
+    uint32_t gpio_rx = CAN_RX;
+    uint32_t gpio_tx = CAN_TX;
+
+    // Setup canbus
+    can2040_setup(&cbus, pio_num);
+    can2040_callback_config(&cbus, can2040_cb);
+
+    // Enable irqs
+    irq_set_exclusive_handler(PIO0_IRQ_0_IRQn, PIOx_IRQHandler);
+    NVIC_SetPriority(PIO0_IRQ_0_IRQn, 1);
+    NVIC_EnableIRQ(PIO0_IRQ_0_IRQn);
+
+    // Start canbus
+    can2040_start(&cbus, sys_clock, bitrate, gpio_rx, gpio_tx);
+}
 
 int main() {
-    // idk...
+    canbus_setup();
+
+    while (1)
+    {
+        struct can2040_msg msg = {
+            .id = 1,
+            .dlc = 1,
+            .data32 = {
+                0xffffffff, 
+                0xffffffff
+            }
+        }
+        can2040_transmit(&cbus, &msg);
+        sleep_ms(1000);
+    }
+    
 }
+
