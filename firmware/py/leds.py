df20319280dd24af4975aff9847dae6b221a16b0
linted <linted@users.noreply.github.com>
2023-07-09 22:41:40 -0400

    updates

--- firmware/py/leds.py
+++ firmware/py/leds.py
@@ -3,16 +3,27 @@ import machine
 
 
 class leds() :
+    # _leds = [
+    #     (True, False, None, None),
+    #     (False, True, None, None),
+    #     (None, True, False, None),
+    #     (None, False, True, None),
+    #     (False, None, True, None),
+    #     (None, None, False, True),
+    #     (None, False, None, True),
+    #     (False, None, None, True),
+    #     (True, None, False, None)
+    # ]
     _leds = [
-        (True, False, None, None),
-        (False, True, None, None),
-        (None, True, False, None),
-        (None, False, True, None),
-        (False, None, True, None),
-        (None, None, False, True),
-        (None, False, None, True),
-        (False, None, None, True),
-        (True, None, False, None)
+        (False, True, None, None), #18
+        (True, False, None, None), #15
+        (None, False, True, None), #22
+        (None, True, False, None), #19
+        (True, None, False, None), #16
+        (None, None, True, False), #23
+        (None, True, None, False), #20
+        (True, None, None, False), #17
+        (False, None, True, None), #21
     ]
 
     _lines = [
@@ -46,5 +57,24 @@ class leds() :
             else:
                 self.count = (self.count+1) % len(self._leds)
         self.itteration += 1
-        
 
+
+# _lines = [
+#     machine.Pin(6, machine.Pin.OUT),
+#     machine.Pin(5, machine.Pin.OUT),
+#     machine.Pin(4, machine.Pin.OUT),
+#     machine.Pin(3, machine.Pin.OUT),
+# ]
+
+
+
+# def do_led(a):
+#     for s, l in zip(a, range(4)):
+#         if s is None:
+#             _lines[l].init(mode=machine.Pin.IN)
+#         else:
+#             _lines[l].init(mode=machine.Pin.OUT)
+#             if s is True:
+#                 _lines[l].high()
+#             else:
+#                 _lines[l].low()
