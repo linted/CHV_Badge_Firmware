b81f1a583e4e93a467ca8a33cb60ea6d20c9b0f3
linted <linted@users.noreply.github.com>
2023-07-16 17:23:04 -0400

    updated with working(ish) example

--- firmware/py/usbd_cdc.py
+++ firmware/py/usbd_cdc.py
@@ -3,22 +3,19 @@ try:
     from usbd import device, cdc
     ud = device.get_usbdevice()
     cdc.setup_CDC_device()
-    ctrl_cdc = cdc.CDCControlInterface('')
-    data_cdc = cdc.CDCDataInterface('')
-    data_cdc2 = cdc.CDCDataInterface('')
+    ctrl_cdc = cdc.CDCControlInterface('cdc interface')
+    data_cdc = cdc.CDCDataInterface('data interface')
     ud.add_interface(ctrl_cdc)
     ud.add_interface(data_cdc)
-    # ud.add_interface(data_cdc2)
     ud.reenumerate()
-    time.sleep(10)
-    print(data_cdc.ep_in)
+    time.sleep(3)
+    # print(data_cdc.ep_in)
     # sending something over CDC
     for i in range(10):
-        data_cdc.write(b'Hello World' + str(i) + '\n')
-        # data_cdc2.write(b'Hello World' + str(i) + '\n')
+        data_cdc.write(b'Hello World' + str(i) + '\r\n')
         time.sleep(0.25)
     # receiving something..
-    print("Got :" + str(data_cdc.read(10)) + ":done")
+    # print("Got :" + str(data_cdc.read(10)) + ":done")
 except Exception as e:
-    time.sleep(5)
+    time.sleep(2)
     raise e
