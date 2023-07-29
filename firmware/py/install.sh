9cac40a6c7b72a9be79fff03d0025928a6cb7fe0
linted <linted@users.noreply.github.com>
2023-07-29 16:03:31 -0400

    Ok, even better install.sh

--- firmware/py/install.sh
+++ firmware/py/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 wait_for_file () {
-    echo "Waiting for $1"
+    echo -e "\033[100DWaiting for $1"
     while [ 1 ]; do
         ls $1 1>/dev/null 2>&1 && break
         sleep 0.5
     done
-    echo "found $1"
+    echo -en "\033[1A\033[100D                         "
 }
 
 while [ 1 ]; do
@@ -14,15 +14,18 @@ while [ 1 ]; do
     echo -e "\033[100D%%%% Connect badge and enter bootloader %%%%"
     echo "press reset + bootsel and release reset first"
     wait_for_file /media/$USER/RPI-RP2
+    echo -en "\033[100D                                                        "
     echo -en "\033[1A\033[100D                                                        "
     echo -en "\033[1A\033[100DFlashing firmware                                       "
     cp firmware.uf2 /media/$USER/RPI-RP2/
+    sleep 2
 
     # flash with python code
     wait_for_file /dev/ttyACM0
     for files in *py usbd/ ; do
         echo -en "\033[100DFlashing $files                                            "
         ampy -p /dev/ttyACM0 put $files
+        sleep 1
     done
 
     # reset to make sure everything worked
